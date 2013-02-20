b91490ca0ef07ea8b627540c680a0ff70acd531b
Phil Lavoie <unexisting@sofar.com>
2013-02-20 11:00:58 -0500

    Moved some stuff around

--- src/config.d
+++ src/config.d
@@ -7,6 +7,7 @@ import std.stdio;
 import std.conv;
 import std.exception;
 import std.container;
+import std.algorithm;
 
 //Type of the integer determining the size of the number of permutations.
 alias NoPerms = size_t;
@@ -95,6 +96,6 @@ void parse( ref Config cfg, string[] cmdArgs ) in {
   //Must provide at least one hash.
   enforce( cfg.useHashesFile != cfg.inlineHash, "expected only one way of providing hashes: through a file or inline" );  
   //Check for minimum and maximum permutations validity.
-  enforce( cfg.minPermutations <= cfg.maxPermutations, "combinations minimum is set to " ~ cfg.minPermutations.to!string ~ " and was expected to be under the maximum: " ~ cfg.maxPermutations.to!string );
+  enforce( cfg.minPermutations <= cfg.maxPermutations, "combinations minimum is set to " ~ cfg.minPermutations.to!string ~ " and was expected to be under the maximum: " ~ cfg.maxPermutations.to!string );  
 }
 
